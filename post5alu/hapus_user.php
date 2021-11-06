bf0eaa544ad53c89b51d68f000cd828e0c791d91
aldisaputra15 <92085685+aldisaputra15@users.noreply.github.com>
2021-11-06 12:59:19 +0800

    Postest temu 5

---
+++ post5alu/hapus_user.php
@@ -0,0 +1,10 @@
+<?php
+	include "koneksi.php";
+	$sql="delete from users where id_user= '$_GET[id]'"; 
+	mysqli_query($koneksi, $sql);
+	mysqli_close($koneksi);
+
+	header('location:tampil_user.php');
+?>
+
+
