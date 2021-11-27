71a8203b9d18e646930f1d6d41311896e1a653d7
aldisaputra15 <92085685+aldisaputra15@users.noreply.github.com>
2021-11-27 10:09:42 +0700

    postest temu 6

---
+++ post6pwd/akademik (2).sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Nov 2021 pada 03.53
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.3.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(5) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jkel` varchar(1) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `tgllhr` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `jkel`, `alamat`, `tgllhr`) VALUES
+('MHS01', 'Siti Aminah', 'P', 'SOLO', '1995-10-01'),
+('MHS02', 'Rita', 'P', 'SOLO', '1999-01-01'),
+('MHS03', 'Amirudin', 'L', 'SEMARANG', '1998-08-11'),
+('MHS04', 'Siti Maryam', 'P', 'JAKARTA', '1995-04-15');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `ttl` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `level` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `password`, `nama`, `ttl`, `email`, `level`) VALUES
+('aldi15', '827ccb0eea8a706c4c34a16891f84e7b', 'Mohammad Aldi Saputra', '2000-01-15', 'aldywatch15@gmail.com', ''),
+('alu123', '827ccb0eea8a706c4c34a16891f84e7b', 'Aldi Alu', '2000-10-19', 'mohammad1800018021@webmail.uad.ac.id', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ post6pwd/form_login.php
@@ -0,0 +1,28 @@
+<?php
+echo 
+"<h2>Login</h2>
+<form method=post action=cek_login.php>
+<table>
+	<tr>
+		<td>Username</td><td> : <input name='id_user' type='text'></td>
+	</tr>
+	<tr>
+		<td>Password</td><td> : <input name='paswd' type='password'></td>
+	</tr>
+	<tr>
+		<td>Email</td><td> : <input name='email' type='text'></td>
+	</tr>
+	<tr>
+		<td>Tanggal Lahir</td><td> : <input name='ttl' type='date'></td>
+	</tr>
+	<tr>
+		<td>Captcha<br>
+		<img src='captcha.php' /></td>
+		<td> : <input name='captcha_code' type='text'></td>
+	</tr>
+	<tr>
+		<td colspan=2><input type='submit' value='LOGIN'></td>
+	</tr>
+</table>
+</form>";
+?>
